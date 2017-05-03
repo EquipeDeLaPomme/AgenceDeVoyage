dae0d42e212c9c8e97e7ae0ca72001c76a2360b4
Damien Lamalle <damien.lamalle@soprasteria.com>
2017-05-03 13:27:29 +0200

    Classe Login fonctionnelle

--- src/main/java/Main/MainDamien.java
+++ src/main/java/Main/MainDamien.java
@@ -1,12 +1,29 @@
 package Main;
 
+import java.util.List;
+
+import dao.AdresseDao;
+import dao.AdresseDaoSql;
+import dao.LoginDAO;
+import dao.LoginDaoSql;
+import model.Adresse;
+import model.Login;
+
 public class MainDamien {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
+		// J'instancie le dao SQL des adresses
+        LoginDAO loginDao = new LoginDaoSql();
+        // J'appelle la méthode findAll pour récupérer toutes les adresses
+        // stockées en BDD
+        List<Login> listeLogin = loginDao.findAll();
+		// Affiche un exemple
+        listeLogin.get(1).afficherInfos();
 		
-		
+        Login login = loginDao.findById(1);
+        login.afficherInfos();
 
 	}
 
